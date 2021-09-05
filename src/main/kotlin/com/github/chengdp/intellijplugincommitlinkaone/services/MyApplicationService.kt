5352627c7b8b67dc2d7aebb136c45c703cafb4f2
GitHub Action <action@github.com>
2021-09-05 03:30:47 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/chengdp/intellijplugincommitlinkaone/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.chengdp.intellijplugincommitlinkaone.services
+
+import com.github.chengdp.intellijplugincommitlinkaone.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/chengdp/intellijplugincommitlinkaone/services/MyProjectService.kt
+++ src/main/kotlin/com/github/chengdp/intellijplugincommitlinkaone/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.chengdp.intellijplugincommitlinkaone.services
 
+import com.github.chengdp.intellijplugincommitlinkaone.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
